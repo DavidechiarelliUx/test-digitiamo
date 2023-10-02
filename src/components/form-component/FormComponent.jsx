c026d3e8e7590acd78bfda4b98fbc631fd088f34
DavidechiarelliUx <chiarellidavide99@gmail.com>
2023-10-02 10:50:09 +0200

    feat: add style first block; and implement URL logic

--- src/components/form-component/FormComponent.jsx
+++ src/components/form-component/FormComponent.jsx
@@ -1,11 +1,33 @@
 import React from "react";
-
+import { useState } from "react";
 import styles from "./FormComponent.module.scss";
 
-const FormComponent = () => {
+const FormComponent = ({onSubmit}) => {
+
+    const [inputUrl, setInputUrl] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!isValidUrl(inputUrl)) {
+            alert("Per favore inserisci un URL valido");
+            return;
+        }
+        onSubmit(inputUrl);
+    };
+    
+    // Funzione di utilità per controllare la validità di un URL
+    const isValidUrl = (string) => {
+        try {
+            new URL(string);
+            return true;
+        } catch (_) {
+            return false;  
+        }
+    };
+    
     return (
         <>
-            <form className={styles.form}>
+            <form className={styles.form} onSubmit={handleSubmit}>
                 <select className={styles.select}>
                     {/* aggiunta dei metodi http*/}
                     <option value="GET">GET</option>
@@ -16,7 +38,12 @@ const FormComponent = () => {
                     <option value="DUMB">DUMB</option>
                 </select>
                 
-                <input type="url" className={styles.input} placeholder="Inserisci URL" />
+                <input type="url"
+                className={styles.input}
+                placeholder="Inserisci URL"
+                value={inputUrl}
+                onChange={(e) => setInputUrl(e.target.value)}
+                />
 
                 <button type="submit" className={styles.button}>SEND</button>
             </form>
--- src/components/infoUrl/UrlInfoBlock.jsx
+++ src/components/infoUrl/UrlInfoBlock.jsx
@@ -1,14 +1,44 @@
 import React from "react";
 
-import styles from "./UrlInfoBlock.module.scss"
+import styles from "./UrlInfoBlock.module.scss";
 
+const UrlInfoBlock = ({ url }) => {
+    let domain = "";
+    let scheme = "";
+    let path = "";
 
-const UrlInfoBlock= () => {
+    try {
+        const parsedUrl = new URL(url);
+        domain = parsedUrl.hostname;
+        scheme = parsedUrl.protocol;
+        path = parsedUrl.pathname;
+    } catch (error) {
+        // Non fare nulla se l'URL non è valido
+    }
+
     return (
-        <>
-        <h1> Block info 1</h1>
-        </>
-    )
-}
+        <div className={styles.container}>
+            <p className={styles.p}>URL INFO</p>
+            <div className={styles.infoItem}>
+                <label>DOMAIN:</label> 
+                <span>{domain || ""}</span>
+            </div>
+            <div className={styles.infoItem}>
+                <label>SCHEME:</label>
+                <span>{scheme || ""}</span>
+            </div>
+            <div className={styles.infoItem}>
+                <label>PATH:</label>
+                <span>{path || ""}</span>
+            </div>
+        </div>
+    );
+};
 
 export default UrlInfoBlock;
+
+
+
+
+
+
