924de8001a0c85c7f6d251c761b846e645f09923
DavidechiarelliUx <chiarellidavide99@gmail.com>
2023-10-03 18:03:05 +0200

    feat/casual share link

--- src/components/share-link-component/ShareLink.jsx
+++ src/components/share-link-component/ShareLink.jsx
@@ -1,14 +1,26 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 import styles from "./ShareLink.module.scss";
 
 const ShareLink = () => {
-    const shareLink = "httprequest.com/1a54da684864";
+    const [shareLink, setShareLink] = useState('');
+
+    useEffect(() => {
+        setShareLink(generateRandomLink());
+    }, []);
+
+    const generateRandomLink = (length = 10) => {
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        let result = '';
+        for (let i = 0; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() * characters.length));
+        }
+        return 'httprequest.com/' + result;
+    }
 
     return (
         <>
             <h1 className={styles.Share}>SHARE</h1>
-            <a  className={styles.Link} href="#" onClick={(e) => e.preventDefault()}>{shareLink}</a>
+            <a className={styles.Link} href="#" onClick={(e) => e.preventDefault()}>{shareLink}</a>
         </>
     );
 };
