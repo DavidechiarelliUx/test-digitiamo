4057f99724adddf2a4438876c5b383dce100035b
DavidechiarelliUx <chiarellidavide99@gmail.com>
2023-10-03 11:38:40 +0200

    feat:create block http 2 ; and fix style scss

--- src/components/HttpRequestBlock2/HttpBlock2.jsx
+++ src/components/HttpRequestBlock2/HttpBlock2.jsx
@@ -1,13 +1,21 @@
 import React from "react";
-
-import styles from "./HttpBlock2.module.scss"
+import styles from "./HttpBlock2.module.scss";
 
 const HttpBlock2 = () => {
     return (
-        <>
-        <h1> blocco 3</h1>
-        </>
-    )
+        <div className={styles.container}>
+            <p className={styles.title}>RESPONSE</p>
+            <div className={styles.infoItem}>
+                <span>HTTP/1.1 200 OK</span>
+            </div>
+            <div className={styles.infoItem}>
+                <span>Date: Mon, 27 Jul 2009</span>
+            </div>
+            <div className={styles.infoItem}>
+                <span>Server: Apache/2.2.14 (Win32)</span>
+            </div>
+        </div>
+    );
 }
 
 export default HttpBlock2;
