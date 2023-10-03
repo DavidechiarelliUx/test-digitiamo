854e6e04ac6e0c09940202b305695894045dd088
DavidechiarelliUx <chiarellidavide99@gmail.com>
2023-10-03 11:52:58 +0200

    feat: add a status code ok to send a valid http

--- src/components/form-component/FormComponent.jsx
+++ src/components/form-component/FormComponent.jsx
@@ -5,14 +5,19 @@ import styles from "./FormComponent.module.scss";
 const FormComponent = ({onSubmit}) => {
 
     const [inputUrl, setInputUrl] = useState("");
+    const [statusMessage, setStatusMessage] = useState("");
+    const [status,setStatus] = useState(null)
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
         if (!isValidUrl(inputUrl)) {
             alert("Per favore inserisci un URL valido");
+            setStatus(null);
             return;
         }
         onSubmit(inputUrl);
+        setStatus({ code: "200", message: "Everything is fine" });
     };
     
     // Funzione di utilità per controllare la validità di un URL
@@ -27,6 +32,12 @@ const FormComponent = ({onSubmit}) => {
     
     return (
         <>
+            {status && (
+                <div className={styles.statusContainer}>
+                    <span className={styles.statusCode}>{status.code}</span>
+                    <span className={styles.statusMessage}>{status.message}</span>
+                </div>
+            )}
             <form className={styles.form} onSubmit={handleSubmit}>
                 <select className={styles.select}>
                     {/* aggiunta dei metodi http*/}
