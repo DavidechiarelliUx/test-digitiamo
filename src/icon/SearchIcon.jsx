ff999fb960557e6af100944bd652f284fb25b6fd
DavidechiarelliUx <chiarellidavide99@gmail.com>
2023-10-06 18:25:14 +0200

    fix:add icon

---
+++ src/icon/SearchIcon.jsx
@@ -0,0 +1,18 @@
+const SearchIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={20}
+    height={20}
+    fill="none"
+    viewBox="0 0 24 24"
+  >
+    <path
+      stroke="#ffff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="m15 15 6 6m-11-4a7 7 0 1 1 0-14 7 7 0 0 1 0 14Z"
+    />
+  </svg>
+)
+export default SearchIcon;
